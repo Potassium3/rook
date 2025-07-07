6eb092090354662582a1ce04c8ede0bca8cf7820
Potassium3 <miles.plowman@gmail.com>
2025-07-07 12:36:30 +0000

    Save

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function gridFill() {
+function updateGrid() {
     const grid = document.getElementById("js-grid-fill")
     const size = 10;
+    let x = 0;
+    let y = 5;
     grid.innerHTML = "";
     for (let i=0; i<size; i++) {
         grid.innerHTML += `<tr id="grid-row-${i}"></tr>`;
         let gridRow = document.getElementById("grid-row-"+i);
         for (let j=0; j<size; j++) {
-            gridRow.innerHTML += "<td></td>";
+            if (i === y && j === x) {
+                gridRow.innerHTML += "<td class='sq-rook'></td>";
+            } else {
+                gridRow.innerHTML += "<td></td>";
+            }
         }
     }
 }
-gridFill();
+updateGrid();
